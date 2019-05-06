69d8021210bb79e4cb577a2f0a31f8c389ada68a
van Beek, Laurens <laurens-vanbeek@uiowa.edu>
2019-05-06 15:47:05 -0500

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -1,24 +1,33 @@
+import os
+import io
 from google.cloud import translate
 from google.cloud import vision
 import anki_vector
 import time
+from PIL import Image
+from google.protobuf.json_format import MessageToJson
 
 def main():
     client = vision.ImageAnnotatorClient()
     translate_client = translate.Client()
     target = 'en'
-    with anki_vector.Robot(enable_camera_feed=True) as robot:
+    with anki_vector.Robot() as robot:
+        robot.camera.init_camera_feed()
         for _ in range(30):
             while not robot.camera.latest_image:
                 time.sleep(1.0)
-            image = robot.camera.latest_image
-            response = client.text_detection(image=image)
-            texts = response.text_annotations
-            translation = translate_client.translate(texts,target_language=target)
+            photo = robot.camera.latest_image.raw_image
+            imgByteArr = io.BytesIO()
+            photo.save(imgByteArr, format='PNG')
+            content = imgByteArr.getvalue()
+            image = vision.types.Image(content=content)
+            response = client.document_text_detection(image=image)
+            texts = response.full_text_annotation.text
+            translation = translate_client.translate(texts, target_language=target)
             print(u'Text: {}'.format(texts))
             print(u'Translation: {}'.format(translation['translatedText']))
-            image.show()
-            robot.say_text(translation)
+            say = u'{}'.format(translation['translatedText'])
+            robot.behavior.say_text(say)
 
 
 if __name__ == "__main__":
